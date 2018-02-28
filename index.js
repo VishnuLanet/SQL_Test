34ea656e48af5522876ec523a5ca97ddfa147678
Vishnu <lanetteam.vishnu@gmail.com>
2018-02-28 12:31:13 +0530

    add db

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(bodyparser.json());
 
 
 // Student
-mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-dyp71.mongodb.net:27017,cluster0-shard-00-01-dyp71.mongodb.net:27017,cluster0-shard-00-02-dyp71.mongodb.net:27017/exam?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+// mongoose.connect("mongodb://admin:admin@cluster0-shard-00-00-dyp71.mongodb.net:27017,cluster0-shard-00-01-dyp71.mongodb.net:27017,cluster0-shard-00-02-dyp71.mongodb.net:27017/exam?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin",
+  mongoose.connect("mongodb://varshaLadha:chhotu1810@ds239648.mlab.com:39648/testdbase",
     (err, ress) => {
       if(!err)
         console.log("DB Connected.");
